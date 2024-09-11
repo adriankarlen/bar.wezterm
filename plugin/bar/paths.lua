da2a7a38287dda47f022b53553039bd6192d6e1f
adriankarlen <adrian.karlen@spp.se>
2024-09-11 12:59:23 +0200

    reafctor: move away from one file solution

---
+++ plugin/bar/paths.lua
@@ -0,0 +1,75 @@
+local wez = require "wezterm"
+local utilities = require "plugin.bar.utilities"
+
+local M = {}
+
+local find_git_dir = function(directory)
+  directory = directory:gsub("~", utilities.home)
+
+  while directory do
+    local handle = io.open(directory .. "/.git/HEAD", "r")
+    if handle then
+      handle:close()
+      directory = directory:match "([^/]+)$"
+      return directory
+    elseif directory == "/" or directory == "" then
+      break
+    else
+      directory = directory:match "(.+)/[^/]*"
+    end
+  end
+
+  return nil
+end
+
+M.get_cwd_hostname = function(pane, search_git_root_instead)
+  local cwd, hostname = "", ""
+  local cwd_uri = pane:get_current_working_dir()
+  if cwd_uri then
+    if type(cwd_uri) == "userdata" then
+      -- Running on a newer version of wezterm and we have
+      -- a URL object here, making this simple!
+
+      ---@diagnostic disable-next-line: undefined-field
+      cwd = cwd_uri.file_path
+      ---@diagnostic disable-next-line: undefined-field
+      hostname = cwd_uri.host or wez.hostname()
+    else
+      -- an older version of wezterm, 20230712-072601-f4abf8fd or earlier,
+      -- which doesn't have the Url object
+      cwd_uri = cwd_uri:sub(8)
+      local slash = cwd_uri:find "/"
+      if slash then
+        hostname = cwd_uri:sub(1, slash - 1)
+        -- and extract the cwd from the uri, decoding %-encoding
+        cwd = cwd_uri:sub(slash):gsub("%%(%x%x)", function(hex)
+          return string.char(tonumber(hex, 16))
+        end)
+      end
+    end
+
+    -- Remove the domain name portion of the hostname
+    local dot = hostname:find "[.]"
+    if dot then
+      hostname = hostname:sub(1, dot - 1)
+    end
+    if hostname == "" then
+      hostname = wez.hostname()
+    end
+
+    if utilities.is_windows then
+      cwd = cwd:gsub("/" .. utilities.home .. "(.-)$", "~%1")
+    else
+      cwd = cwd:gsub(utilities.home .. "(.-)$", "~%1")
+    end
+
+    ---search for the git root of the project if specified
+    if search_git_root_instead then
+      local git_root = find_git_dir(cwd)
+      cwd = git_root or cwd ---fallback to cwd
+    end
+  end
+
+  return cwd, hostname
+end
+return M
---
+++ plugin/bar/utilities.lua
@@ -0,0 +1,59 @@
+local H = {}
+
+local wez = require "wezterm"
+
+H.home = (os.getenv "USERPROFILE" or os.getenv "HOME" or wez.home_dir or ""):gsub("\\", "/")
+H.is_windows = package.config:sub(1, 1) == "\\"
+
+
+-- get basename for dir/file, removing ft and path
+H._basename = function(s)
+  if type(s) ~= "string" then
+    return nil
+  end
+  return s:gsub("(.*[/\\])(.*)%.(.*)", "%2")
+end
+
+-- add spaces to each side of a string
+H._space = function(s, space, trailing_space)
+  if type(s) ~= "string" or type(space) ~= "number" then
+    return ""
+  end
+  local spaces = string.rep(" ", space)
+  local trailing_spaces = spaces
+  if trailing_space ~= nil then
+    trailing_spaces = string.rep(" ", trailing_space)
+  end
+  return spaces .. s .. trailing_spaces
+end
+
+-- trim string from trailing spaces and newlines
+H._trim = function(s)
+  return s:match "^%s*(.-)%s*$"
+end
+
+-- merges two tables
+function H._merge(t1, t2)
+  for k, v in pairs(t2) do
+    if type(v) == "table" then
+      if type(t1[k] or false) == "table" then
+        H._merge(t1[k] or {}, t2[k] or {})
+      else
+        t1[k] = v
+      end
+    else
+      t1[k] = v
+    end
+  end
+  return t1
+end
+
+-- return string with spacing adjusted to prev string
+H._constant_width = function(prev, next)
+  local spacing = #prev - #next
+  local first_half = math.floor(spacing / 2)
+  local second_half = math.ceil(spacing / 2)
+  return H._space(next, first_half, second_half)
+end
+
+return H
