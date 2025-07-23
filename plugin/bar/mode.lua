d41ef17f21247a26cdbf82c401ca69a225f9d871
Hikari Sakamoto <hikarisakamoto@gmail.com>
2025-07-23 22:55:46 +0100

    Updated mode

--- plugin/bar/mode.lua
+++ plugin/bar/mode.lua
@@ -1,33 +1,17 @@
----@private
----@class bar.mode
----@field get_mode fun(window: window): string
 local M = {}
 
-
---- Return active mode
----@param window window  
+---Return active key table mode as a user friendly name
+---@param window table
 ---@return string
 M.get_mode = function(window)
-    local key_table = window:active_key_table()
-    if not key_table then
-        return "default"
-    end
-    if key_table == "copy_mode" then
-        return "copy"
-        -- TODO: CHECK MODES AND UPDATE
-        -- elseif key_table == "paste_mode" then
-        --     return "paste"
-        -- elseif key_table == "command_mode" then
-        --     return "command"
-        -- elseif key_table == "search_mode" then
-        --     return "search"
-        -- elseif key_table == "resize_mode" then
-        --     return "resize"
-        -- elseif key_table == "scroll_mode" then
-        --     return "scroll"
-    end
-
-    return key_table
+  local key_table = window:active_key_table()
+  if not key_table then
+    return ""
+  end
+  if key_table == "copy_mode" then
+    return "copy"
+  end
+  return key_table
 end
 
 return M
--- plugin/init.lua
+++ plugin/init.lua
@@ -129,10 +129,10 @@ wez.on("update-status", function(window, pane)
   table.insert(left_cells, { Text = string.rep(" ", options.padding.left) })
   
 if options.modules.mode.enabled then
-    local active = mode.get_mode(window)
+ local active = mode.get_mode(window)
     if #active > 0 then
-        table.insert(left_cells, {Foreground = {Color = palette.ansi[options.modules.mode.color]}})
-        table.insert(left_cells, {Text = options.modules.mode.icon .. utilities._space(active, options.separator.space)})
+      table.insert(left_cells, { Foreground = { Color = palette.ansi[options.modules.mode.color] } })
+      table.insert(left_cells, { Text = options.modules.mode.icon .. utilities._space(active, options.separator.space) })
     end
 end
 
